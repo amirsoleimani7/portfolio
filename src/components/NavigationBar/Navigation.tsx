ea0d422626b664a33b04525d5fa53056d3538181
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-06-13 23:56:33 +0330

    add : chore : cleaning up

--- src/components/NavigationBar/Navigation.tsx
+++ src/components/NavigationBar/Navigation.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import navigationData, {
   headerButton,
 } from "../../storage/data/navigationData";
@@ -6,12 +6,20 @@ import { buildProjectionTransform } from "framer-motion";
 
 const Navigation = () => {
   
+
   return (
-    <div className="flex gap-4 p-3  border border-gray-800 rounded-3xl text-white font-Lato">
+    
+    <div
+      className="flex justify-center gap-4 p-3 border border-gray-700 rounded-3xl text-white font-Lato transition-all duration-200"
+    > 
       {navigationData.map((btn: headerButton, index: number) => {
+        // making icon as component type so it would't through the error
         let Icon = btn?.Icon as React.ComponentType;
         return (
-          <button key={btn.id} className="flex items-center  gap-1 hover:text-sky-400 transition-colors duration-200">
+          <button
+            key={btn.id}
+            className="flex items-center  gap-1 hover:text-sky-400 transition-colors duration-200"
+          >
             <Icon />
             <p>{btn.title}</p>
           </button>
@@ -20,6 +28,5 @@ const Navigation = () => {
     </div>
   );
 };
-
 
 export default Navigation;
