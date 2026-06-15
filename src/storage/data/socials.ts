8887890c4dae74064bc77d6db7c4f3e8b7be72d0
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-06-15 15:09:44 +0330

    add : added links to socials

--- src/storage/data/socials.ts
+++ src/storage/data/socials.ts
@@ -8,23 +8,29 @@ import { IconType } from "react-icons";
 export type SocialsType = {
     id : number,
     name : string,
-    logo : IconType
+    logo : IconType,
+    link : string
 }
 
 export const Socials: SocialsType[] = [
     {
         id : 1,
         name : "instagram",
-        logo : FaInstagram
+        logo : FaInstagram,
+        link : "https://www.instagram.com/amirhossein_soleimani7/"
+        
     },
     {
         id : 2,
         name : "github",
-        logo : FaGithub
+        logo : FaGithub,
+        link : "https://github.com/amirsoleimani7/"
     },
     {
         id : 3,
         name : "linkedin",
-        logo : FaLinkedin
+        logo : FaLinkedin,
+        link : "www.linkedin.com/in/amir-hossein-soleimani-b2a603227"
     },
 ]
+
