74f2ef5a5d4592b5b7c10979dda02c38a735468f
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-06-15 17:39:59 +0330

    add : backdrop of blur added to the background

--- src/components/NavigationBar/Navigation.tsx
+++ src/components/NavigationBar/Navigation.tsx
@@ -5,7 +5,7 @@ import { userInformation } from "../../storage/data/Information";
 
 const Navigation = () => {
   return (
-    <div className="w-fit px-3 py-3 flex justify-center  rounded-full gap-5 border border-gray-800 my-2">
+    <div className="w-fit px-3 py-3 flex justify-center  rounded-full gap-5 border border-gray-800 my-2 backdrop-blur-xl z-10">
       <div className="flex justify-center items-center gap-2 text-white mr-3 cursor-pointer">
         <div className="w-7 h-7 rounded-full overflow-hidden">
           <img src={userInformation.profile} alt="user profile" />
