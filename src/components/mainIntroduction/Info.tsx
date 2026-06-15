23131b0527e964d2e82fdf32c90f3ecb7e7b4a97
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-06-15 14:38:12 +0330

    add : socials files added

--- src/components/mainIntroduction/Info.tsx
+++ src/components/mainIntroduction/Info.tsx
@@ -2,11 +2,27 @@ import { userInformation } from "../../storage/data/Information";
 
 export const Info = () => {
   console.log(userInformation);
+
   return (
-      <>
-        <div className="h-[500px]">
+    <>
+      <div className="flex bg-green-100 h-[500px] w-full p-[5%]">
+        <div className="w-[60%] h-full bg-red-100 ">
+          <h1 className="text-6xl font-bold">Hi I'm Amir</h1>
+          <h1 className="text-6xl font-bold">Welcome To my portpolio</h1>
+          <p>
+            I'm a Frontend Enginner with 3+ years of experience, knowen for
+            strong attention to details and pixel perfect exceution of the
+            projects
+          </p>
+          <p>
+            My main tech stack it React, Javascript and typescript, and modern
+            front-end technologies for building web apps
+          </p>
+        </div>
+        <div className="w-[40%] h-full bg-white">
           
         </div>
-      </>
-    );  
+      </div>
+    </>
+  );
 };
---
+++ src/storage/data/socials.ts
@@ -0,0 +1,30 @@
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
+
+import { IconType } from "react-icons";
+
+type SocialsType = {
+    id : number,
+    name : string,
+    logo : IconType
+}
+
+export const Socials: SocialsType[] = [
+    {
+        id : 1,
+        name : "instagram",
+        logo : FaInstagram
+    },
+    {
+        id : 2,
+        name : "github",
+        logo : FaGithub
+    },
+    {
+        id : 3,
+        name : "linkedin",
+        logo : FaLinkedin
+    },
+]
