441c01dc5adf31afa73065834375ae6bdfd86901
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-06-17 03:02:52 +0330

    add : blur added, kinda

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import { TechStack } from "./components/techStack/TeckStack";
 import { ReactLenis } from 'lenis/react'
 import GithubActivity from "./components/githubActivity/GithubActivity";
 
+
+import "./components/footer/footerBlur.css"
+
+
 function App() {
   return (
     <div
@@ -36,6 +40,16 @@ function App() {
           <div className="w-[60%] flex justify-center border-r border-l border-gray-800 transition-all duration-200 ease-in-out max-lg:w-[95%] max-2xl:w-[80%]">
               <GithubActivity/>
           </div>
+        </div>
+        
+                <div className="w-full flex justify-center h-fit border-b border-gray-800 ">
+          <div className="w-[60%] flex justify-center border-r border-l border-gray-800 transition-all duration-200 ease-in-out max-lg:w-[95%] max-2xl:w-[80%]">
+            <Info />
+          </div>
+        </div>
+
+
+        <div className="w-full h-[50px] z-50 fixed bottom-0 backdrop-blur-[1px] custom-blur border-none outline-none">
         </div>
       </div>
   );
