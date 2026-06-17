c0dc1740c4cd08d25f735490e42ca64c02451bc8
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-06-17 20:27:08 +0330

    add : simple animation on hover

--- src/components/Projects/ProjectInstance.tsx
+++ src/components/Projects/ProjectInstance.tsx
@@ -11,13 +11,13 @@ export const ProjectInstance: React.FC<CustomType> = (props) => {
   console.log(is_last);
   
   return (
-    <div className={`w-full h-[100px] items-center flex justify-between text-white`}>
+    <div className={`w-full h-[100px] items-center flex justify-between text-white group`}>
       <div className="w-[30%]">
-        <h1 className="text-xl letter-spacing font-bold tracking-wide">{props.projectName}</h1>
-        <h1 className="text-sm text-gray-500">{props.ProjectYear}</h1>
+        <h1 className="text-xl letter-spacing font-bold tracking-wide group-hover:translate-x-20 transition-all duration-500 ease-in-out">{props.projectName}</h1>
+        <h1 className="text-sm text-gray-500 group-hover:translate-x-20 transition-all duration-300 ease-in-out">{props.ProjectYear}</h1>
       </div>
       
-      <div className="w-[30%]  flex flex-wrap">
+      <div className="w-[30%] flex flex-wrap group-hover:-translate-x-20 duration-300 transition-all ease-in-out">
         {props.tecksUsed.map((p, index) => {
           return (
             <div
@@ -25,7 +25,7 @@ export const ProjectInstance: React.FC<CustomType> = (props) => {
               className="px-2 py-1 m-1 flex justify-center items-center outline outline-1 outline-gray-700 gap-2 rounded-full text-gray-400  transition-all duration-200 ease-in-out hover:text-white hover:outline-white cursor-pointer"
             >
               <p.icon />
-              <p className="">{p.text}</p>
+              <p className="max-md:hidden :">{p.text}</p>
             </div>
           );
         })}
