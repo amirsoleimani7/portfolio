dffe6b3dbaee456dda6c0f9f3c00e3ff4257e590
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-06-18 01:16:23 +0330

    chore : cleaning up before movingon

---
+++ src/components/Projects/FloatingPreview.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { motion } from "motion/react";
+
+export interface PositionType {
+  is_active: boolean;
+  x: number;
+  y: number;
+}
+
+export const FloatingPreview: React.FC<PositionType> = (prop: PositionType) => {
+  return (
+    <motion.div
+      className="w-[100px] h-[100px]  bg-red-500 fixed pointer-events-none transition-all duration-200 ease-in-out"
+      animate={{
+        left: prop.x,
+        top: prop.y,
+        opacity: `${prop.is_active ? 1 : 0}`,
+      }}
+    />
+  );
+};
--- src/components/Projects/ProjectInstance.tsx
+++ src/components/Projects/ProjectInstance.tsx
@@ -1,5 +1,6 @@
-import { MouseEvent, useEffect, useState } from "react";
+import { MouseEvent, useRef, useState } from "react";
 import { ProjectInformationType } from "../../storage/data/ProjectsInformation";
+import { FloatingPreview, PositionType } from "./FloatingPreview";
 
 type CustomType = ProjectInformationType & {
   is_last: boolean;
@@ -7,31 +8,62 @@ type CustomType = ProjectInformationType & {
 
 export const ProjectInstance: React.FC<CustomType> = (props) => {
   const { is_last, ...projectProps } = props;
-  const [cordinates, setCordinates] = useState<{
-    x: number;
-    y: number;
-  }>({
+
+  const cordinatesRef = useRef<PositionType>({
+    is_active: false,
     x: 0,
     y: 0,
   });
 
-  
+  // const [cordinates, setCordinates] = useState<PositionType>({
+  //   is_active: false,
+  //   x: 0,
+  //   y: 0,
+  // });
+
   const handle_mouse_move = (e: MouseEvent) => {
     const x: number = e.clientX;
     const y: number = e.clientY;
-    setCordinates({
-      x : x,
-      y : y
-    })
+
+    cordinatesRef.current = {
+      is_active: true,
+      x: x,
+      y: y,
+    };
+
+    // setCordinates({
+    //   is_active: true,
+    //   x: x,
+    //   y: y,
+    // });
+    console.log(cordinatesRef.current);
   };
-  
+
+  const handle_mouse_leave = (e: MouseEvent) => {
+    // setCordinates({
+    //   is_active: false,
+    //   x: 0,
+    //   y: 0,
+    // });
+
+    // const x: number = e.clientX;
+    // const y: number = e.clientY;
+
+    cordinatesRef.current = {
+      is_active: false,
+      x: 0,
+      y: 0,
+    };
+  };
+
   return (
     <div
-      className={`w-full h-[100px] items-center flex justify-between text-white group cursor-pointer`}
+      className={`w-full h-[100px] items-center flex justify-between text-white group cursor-pointer relative`}
       onMouseOver={handle_mouse_move}
-      
-
+      onMouseLeave={handle_mouse_leave}
     >
+      <FloatingPreview {...cordinatesRef.current} />
+
       <div className="w-[30%]">
         <h1 className="text-xl letter-spacing font-bold tracking-wide group-hover:translate-x-20 transition-all duration-500 ease-in-out">
           {props.projectName}
