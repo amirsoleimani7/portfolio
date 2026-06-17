737fc5d7134657ff1432c515072c551bc6d44700
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-06-17 15:45:41 +0330

    chore : cleaning up

--- src/App.tsx
+++ src/App.tsx
@@ -11,55 +11,56 @@ import GradualBlur from "./components/footer/GradualBlur";
 
 function App() {
   return (
-    <div
-      className="w-screen p-0 m-0 bg-main-color justify-start
-    flex flex-col relative"
-    >
-      <ReactLenis root />
+    
+      <div
+        className="w-screen p-0 m-0 bg-main-color justify-start
+      flex flex-col relative"
+      >
+        <ReactLenis root />
 
-      <div className="w-full flex justify-center h-fit border-b border-gray-800">
-        <div className="w-[60%] flex justify-center border-r border-l border-gray-800 transition-all duration-200 ease-in-out max-lg:w-[95%] max-2xl:w-[80%]">
-          <Navigation />
+        <div className="w-full flex justify-center h-fit border-b border-gray-800">
+          <div className="w-[60%] flex justify-center border-r border-l border-gray-800 transition-all duration-200 ease-in-out max-lg:w-[95%] max-2xl:w-[80%]">
+            <Navigation />
+          </div>
+        </div>
+
+        <div className="w-full flex justify-center h-fit border-b border-gray-800 ">
+          <div className="w-[60%] flex justify-center border-r border-l border-gray-800 transition-all duration-200 ease-in-out max-lg:w-[95%] max-2xl:w-[80%]">
+            <Info />
+          </div>
+        </div>
+
+        <div className="w-full  flex justify-center  border-b border-gray-800 ">
+          <div className="w-[60%] flex justify-center border-r border-l border-gray-800 transition-all duration-200 ease-in-out max-lg:w-[95%] max-2xl:w-[80%]">
+            <TechStack />
+          </div>
+        </div>
+
+        <div className="w-full  flex justify-center  border-b border-gray-800">
+          <div className="w-[60%] flex justify-center border-r border-l border-gray-800 transition-all duration-200 ease-in-out max-lg:w-[95%] max-2xl:w-[80%]">
+            <GithubActivity />
+          </div>
+        </div>
+
+        <div className="w-full  flex justify-center  border-b border-gray-800 bg-white">
+          <div className="w-[60%] flex justify-center  border-gray-800 transition-all duration-200 ease-in-out max-lg:w-[95%] max-2xl:w-[80%]">
+            <Footer />
+          </div>
+        </div>
+
+        <div className="w-full h-[50px] z-50 fixed bottom-0 border-none outline-none">
+          <GradualBlur
+            target="parent"
+            position="bottom"
+            height="7rem"
+            strength={2}
+            divCount={5}
+            curve="bezier"
+            exponential
+            opacity={1}
+          />
         </div>
       </div>
-
-      <div className="w-full flex justify-center h-fit border-b border-gray-800 ">
-        <div className="w-[60%] flex justify-center border-r border-l border-gray-800 transition-all duration-200 ease-in-out max-lg:w-[95%] max-2xl:w-[80%]">
-          <Info />
-        </div>
-      </div>
-
-      <div className="w-full  flex justify-center  border-b border-gray-800 ">
-        <div className="w-[60%] flex justify-center border-r border-l border-gray-800 transition-all duration-200 ease-in-out max-lg:w-[95%] max-2xl:w-[80%]">
-          <TechStack />
-        </div>
-      </div>
-
-      <div className="w-full  flex justify-center  border-b border-gray-800">
-        <div className="w-[60%] flex justify-center border-r border-l border-gray-800 transition-all duration-200 ease-in-out max-lg:w-[95%] max-2xl:w-[80%]">
-          <GithubActivity />
-        </div>
-      </div>
-
-      <div className="w-full  flex justify-center  border-b border-gray-800 bg-white">
-        <div className="w-[60%] flex justify-center  border-gray-800 transition-all duration-200 ease-in-out max-lg:w-[95%] max-2xl:w-[80%]">
-          <Footer />
-        </div>
-      </div>
-
-      <div className="w-full h-[50px] z-50 fixed bottom-0 border-none outline-none">
-        <GradualBlur
-          target="parent"
-          position="bottom"
-          height="7rem"
-          strength={2}
-          divCount={5}
-          curve="bezier"
-          exponential
-          opacity={1}
-        />
-      </div>
-    </div>
   );
 }
 
