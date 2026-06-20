6b2427226d23c2499b90690940e187a2d34b2375
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-06-20 10:48:23 +0330

    remove : removing project float items

--- src/components/Projects/ProjectInstance.tsx
+++ src/components/Projects/ProjectInstance.tsx
@@ -1,69 +1,16 @@
-import { MouseEvent, useRef, useState } from "react";
 import { ProjectInformationType } from "../../storage/data/ProjectsInformation";
-import { FloatingPreview, PositionType } from "./FloatingPreview";
 
 type CustomType = ProjectInformationType & {
   is_last: boolean;
 };
 
 export const ProjectInstance: React.FC<CustomType> = (props) => {
-  const { is_last, ...projectProps } = props;
-
-  const cordinatesRef = useRef<PositionType>({
-    is_active: false,
-    x: 0,
-    y: 0,
-  });
-
-  // const [cordinates, setCordinates] = useState<PositionType>({
-  //   is_active: false,
-  //   x: 0,
-  //   y: 0,
-  // });
-
-  const handle_mouse_move = (e: MouseEvent) => {
-    const x: number = e.clientX;
-    const y: number = e.clientY;
-
-    cordinatesRef.current = {
-      is_active: true,
-      x: x,
-      y: y,
-    };
-
-    // setCordinates({
-    //   is_active: true,
-    //   x: x,
-    //   y: y,
-    // });
-    console.log(cordinatesRef.current);
-  };
-
-  const handle_mouse_leave = (e: MouseEvent) => {
-    // setCordinates({
-    //   is_active: false,
-    //   x: 0,
-    //   y: 0,
-    // });
-
-    // const x: number = e.clientX;
-    // const y: number = e.clientY;
-
-    cordinatesRef.current = {
-      is_active: false,
-      x: 0,
-      y: 0,
-    };
-  };
-
+  
   return (
     <div
       className={`w-full h-[100px] items-center flex justify-between text-white group cursor-pointer relative`}
-      onMouseOver={handle_mouse_move}
-      onMouseLeave={handle_mouse_leave}
     >
-      <FloatingPreview {...cordinatesRef.current} />
-
+      
       <div className="w-[30%]">
         <h1 className="text-xl letter-spacing font-bold tracking-wide group-hover:translate-x-20 transition-all duration-500 ease-in-out">
           {props.projectName}
