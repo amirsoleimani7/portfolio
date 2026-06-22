9a280d0e42d623b874a92c1a9fd81cfb59c72a01
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-06-22 23:40:43 +0330

    add : parsing xml to js

--- src/components/spotify/Spotify.tsx
+++ src/components/spotify/Spotify.tsx
@@ -1,28 +1,61 @@
 import axios from "axios";
-import XMLParser from 'react-xml-parser';
+import React, { useEffect, useState } from "react";
+import xml2js from "xml2js";
 
 export const Spotify = () => {
-  // we need to fetch the data from spotfiy
+  type trackType = {
+    trackName: string;
+    trackArtist: string;
+    date: string;
+    album: string;
+    trackIamge : string;
+  };
+  
+  const [lastTrack, setLastTrack] = useState<trackType>({
+    trackName: "test",
+    trackArtist: "test",
+    date: "test",
+    album: "test",
+    trackImage : "someth",
+  });
 
-  //Application name	amir
-  //API key	1d06aaf516d9d0cfb6ed960c8d5584b2
-  //Shared secret	778490b53e2aad44e0a59db28f0279ee
-  //Registered to	amirsoleimani
+  const [data, setData] = useState<string>("");
 
   const handle_update = async () => {
-    const res = await axios.get(
-      "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks",{
+    await axios
+      .get("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks", {
         params: {
           api_key: "1d06aaf516d9d0cfb6ed960c8d5584b2",
           limit: 1,
           user: "amirsoleimani",
         },
-      },
-    );
-    
-    console.log(res.data);
+      })
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
+  useEffect(() => {
+    const parser = new xml2js.Parser();
+    parser.parseString(data, function (err, result) {
+      if (result) {
+        const last_track = result.lfm.recenttracks[0].track[0];
+        console.log(last_track);
+        setLastTrack({
+          trackName: last_track.name[0],
+          trackArtist: last_track.artist[0]._,
+          date: last_track.date[0]._,
+          album: last_track.album[0]._,
+          trackIamge : last_track.image[3]._
+        });
+      }
+    });
+  }, [data]);
+
+  
   return (
     <div className="w-full h-20 p-[5%]">
       <button onClick={handle_update} className="w-20 h-20 bg-yellow-200">
