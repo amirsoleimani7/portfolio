53c2b5f4120c8c9c6c92e7ffdeda8600c5e89725
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-06-23 00:24:10 +0330

    add : boilerplate component for lastTrack added

--- src/components/spotify/Spotify.tsx
+++ src/components/spotify/Spotify.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import xml2js from "xml2js";
+import { FaLastfm } from "react-icons/fa";
 
 export const Spotify = () => {
   type trackType = {
@@ -8,7 +9,8 @@ export const Spotify = () => {
     trackArtist: string;
     date: string;
     album: string;
-    trackIamge: string;
+    trackImage: string;
+    link : string
   };
 
   const [lastTrack, setLastTrack] = useState<trackType>({
@@ -16,7 +18,8 @@ export const Spotify = () => {
     trackArtist: "test",
     date: "test",
     album: "test",
-    trackImage: "someth",
+    trackImage: "test",
+    link : "test"
   });
 
   const [data, setData] = useState<string>("");
@@ -51,7 +54,8 @@ export const Spotify = () => {
           trackArtist: last_track.artist[0]._,
           date: last_track.date[0]._,
           album: last_track.album[0]._,
-          trackIamge: last_track.image[3]._,
+          trackImage: last_track.image[3]._,
+          link : last_track.url[0],
         });
       }
     });
@@ -66,17 +70,33 @@ export const Spotify = () => {
           Last song <span className="text-white">Played</span>
         </h1>
       </div>
-      <div className="w-full h-[100px] bg-red-100">
-          <div className="flex w-1/3 h-full bg-yellow-200">
+      <div className="w-full h-[100px] flex justify-between">
+        <div className="flex w-1/3 h-full gap-5 text-white">
           <div className="w-50 h-50 rounded-full bg-green-200 overflow-hidden">
-            <img src={lastTrack.trackIamge} alt="tracks profile pic" className="w-full h-full" />
+            <img
+              src={lastTrack.trackImage}
+              alt="tracks profile pic"
+              className="w-full h-full"
+            />
           </div>
-            <div className="flex flex-col">
-              <h1 className="">{lastTrack.trackName}</h1>
-              <h1 className="">{lastTrack.album}</h1>
-              <h1 className="">{lastTrack.trackArtist}</h1>
-            </div>
+          <div className="flex flex-col justify-center gap-1">
+            <h1 className="font-bold">{lastTrack.trackArtist}</h1>
+            <h1 className="font-semibold text-gray-500">
+              {lastTrack.trackName}
+            </h1>
           </div>
+        </div>
+
+        <div className="h-full flex items-center ">
+          <a href={lastTrack.link} target="_blank" className=" rounded-full px-3 py-2 border-none outline outline-1 outline-gray-600 duration-200 transition-all ease-in-out hover:outline-white group">
+            <button className=" gap-5 flex items-center justify-between ">
+              <h1 className="duration-200 transition-all ease-in-out group-hover:text-white text-gray-400">Listen on Last.fm </h1>
+              <div className="rounded-full duration-200 transition-all ease-in-out group-hover:bg-white bg-gray-400 text-red-600 p-1">
+                <FaLastfm />
+              </div>
+            </button>
+          </a>
+        </div>
       </div>
     </div>
   );
