da9955129da7e181c62487ae0fca0b1f83b3c4bc
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-06-22 23:52:12 +0330

    add : boilerplate lasttrack card

--- src/components/spotify/Spotify.tsx
+++ src/components/spotify/Spotify.tsx
@@ -8,37 +8,39 @@ export const Spotify = () => {
     trackArtist: string;
     date: string;
     album: string;
-    trackIamge : string;
+    trackIamge: string;
   };
-  
+
   const [lastTrack, setLastTrack] = useState<trackType>({
     trackName: "test",
     trackArtist: "test",
     date: "test",
     album: "test",
-    trackImage : "someth",
+    trackImage: "someth",
   });
 
   const [data, setData] = useState<string>("");
 
-  const handle_update = async () => {
-    await axios
-      .get("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks", {
-        params: {
-          api_key: "1d06aaf516d9d0cfb6ed960c8d5584b2",
-          limit: 1,
-          user: "amirsoleimani",
-        },
-      })
-      .then((res) => {
-        setData(res.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
   useEffect(() => {
+    async function getTrack() {
+      await axios
+        .get("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks", {
+          params: {
+            api_key: "1d06aaf516d9d0cfb6ed960c8d5584b2",
+            limit: 1,
+            user: "amirsoleimani",
+          },
+        })
+        .then((res) => {
+          setData(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+
+    getTrack();
+
     const parser = new xml2js.Parser();
     parser.parseString(data, function (err, result) {
       if (result) {
@@ -49,18 +51,33 @@ export const Spotify = () => {
           trackArtist: last_track.artist[0]._,
           date: last_track.date[0]._,
           album: last_track.album[0]._,
-          trackIamge : last_track.image[3]._
+          trackIamge: last_track.image[3]._,
         });
       }
     });
   }, [data]);
 
-  
+  console.log(lastTrack);
+
   return (
-    <div className="w-full h-20 p-[5%]">
-      <button onClick={handle_update} className="w-20 h-20 bg-yellow-200">
-        get update
-      </button>
+    <div className="w-full  p-[5%] flex flex-col">
+      <div className=" w-full flex items-center justify-start mb-5">
+        <h1 className="text-2xl font-bold text-gray-500 max-md:text-xl">
+          Last song <span className="text-white">Played</span>
+        </h1>
+      </div>
+      <div className="w-full h-[100px] bg-red-100">
+          <div className="flex w-1/3 h-full bg-yellow-200">
+          <div className="w-50 h-50 rounded-full bg-green-200 overflow-hidden">
+            <img src={lastTrack.trackIamge} alt="tracks profile pic" className="w-full h-full" />
+          </div>
+            <div className="flex flex-col">
+              <h1 className="">{lastTrack.trackName}</h1>
+              <h1 className="">{lastTrack.album}</h1>
+              <h1 className="">{lastTrack.trackArtist}</h1>
+            </div>
+          </div>
+      </div>
     </div>
   );
 };
