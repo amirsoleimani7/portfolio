e04ca62841aa0f03c1ca61f257c247dc3e59d51c
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-06-27 20:43:56 +0330

    add : fix styling a little

--- src/components/Projects/FloatingPreview.tsx
+++ src/components/Projects/FloatingPreview.tsx
@@ -2,29 +2,24 @@ import React from "react";
 import { motion } from "motion/react";
 
 export interface PositionType {
-  imageUrl : string,
+  imageUrl: string;
   is_active: boolean;
   x?: number;
   y?: number;
 }
 
 export const FloatingPreview: React.FC<PositionType> = (prop: PositionType) => {
-  
   return (
-    <motion.div 
-      className="w-[100px] h-[100px]  bg-red-500 fixed pointer-events-none transition-all  ease-linear"
-      
+    <motion.div
+      className="w-[300px] h-[150px]  bg-red-500 fixed pointer-events-none transition-all  ease-linear overflow-hidden rounded-xl z-50"
       animate={{
         left: prop.x,
         top: prop.y,
-        transition : {duration : .2},
+        transition: { duration: 0.05 },
         opacity: `${prop.is_active ? "1" : "0"}`,
       }}
     >
-
-      <img src={prop.imageUrl} alt="" className="w-full h-full" />
-
+      <img src={prop.imageUrl} alt="" className="w-full h-full object-cover" />
     </motion.div>
-    
-  );  
+  );
 };
