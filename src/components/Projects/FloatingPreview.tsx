b1e48749ceacee5d1f570214c11dc2fd3846e01a
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-06-30 18:46:00 +0330

    chore : cleaning up

--- src/components/Projects/FloatingPreview.tsx
+++ src/components/Projects/FloatingPreview.tsx
@@ -4,26 +4,20 @@ import Skeleton from "react-loading-skeleton";
 
 export interface PositionType {
   currentIndex: number[];
-  allImages : string[];
+  allImages: string[];
   x?: number;
   y?: number;
 }
 
 export const FloatingPreview: React.FC<PositionType> = (prop: PositionType) => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [lastIndex, setLastIndex] = useState<number>(0);
   const [transition, setTransition] = useState<boolean>(true);
 
-  useEffect(() => {
-    
-  }, [prop.currentIndex]);
-  
-  console.log(prop.currentIndex);
-  
+  useEffect(() => {}, [prop.currentIndex]);
 
   return (
     <motion.div
-      className="w-[350px] h-[200px]  fixed pointer-events-none  rounded-xl z-50 bg-gray-800 scale-0"
+      className="w-[350px] h-[200px] fixed pointer-events-none  rounded-xl z-50 bg-gray-800 scale-0 "
       animate={{
         scale: 1,
         left: prop.x,
@@ -31,33 +25,35 @@ export const FloatingPreview: React.FC<PositionType> = (prop: PositionType) => {
         transition: { duration: 0.1 },
       }}
     >
-      <img
-        src={prop.allImages[prop.currentIndex[1]]}
-        className={`w-full h-full object-cover transition-all  duration-1000 translate-x-full`}
-        alt="projects sample"
-        onLoad={() => {
-          console.log("loaded");
-          setIsLoading(false);
-        }}
-        onError={() => {
-          console.log("error loading image");
-          setIsLoading(true);
-        }}
-      />
-      
-      <img
-        src={prop.allImages[prop.currentIndex[0]]}
-        className={`w-full h-full object-cover transition-all  duration-1000 translate-x-full`}
-        alt="projects sample"
-        onLoad={() => {
-          console.log("loaded");
-          setIsLoading(false);
-        }}
-        onError={() => {
-          console.log("error loading image");
-          setIsLoading(true);
-        }}
-      />
+      <div className="w-full h-full relative">
+        <img
+          src={prop.allImages[prop.currentIndex[1]]}
+          className={`w-full h-full object-cover transition-all  duration-100 absolute `}
+          alt="projects sample"
+          onLoad={() => {
+            console.log("loaded");
+            setIsLoading(false);
+          }}
+          onError={() => {
+            console.log("error loading image");
+            setIsLoading(true);
+          }}
+        />
+
+        <img
+          src={prop.allImages[prop.currentIndex[0]]}
+          className={`w-full h-full object-cover transition-all  duration-100 absolute `}
+          alt="projects sample"
+          onLoad={() => {
+            console.log("loaded");
+            setIsLoading(false);
+          }}
+          onError={() => {
+            console.log("error loading image");
+            setIsLoading(true);
+          }}
+        />
+      </div>
 
       {isLoading && (
         <div className="w-full h-full absolute top-0 left-0">
