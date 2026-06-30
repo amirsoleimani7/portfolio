9e652de373272e987b97855e9471f8285091119a
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-06-30 21:12:14 +0330

    chore : cleaning up

--- src/components/Projects/ProjectInstance.tsx
+++ src/components/Projects/ProjectInstance.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { ProjectInformationType } from "../../storage/data/ProjectsInformation";
 
 type CustomType = ProjectInformationType & {
