313ed1c292d8351bf8aa17e7b84ad7ac8d441530
amirsoleimani7 <a.soleimani@ec.iut.ac>
2026-08-12 23:37:32 +0330

    remove : performance damaging code removed

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,6 @@ import { Footer } from "./components/footer/Footer";
 import GradualBlur from "./components/footer/GradualBlur";
 import { Projects } from "./components/Projects/Projects";
 import { Spotify } from "./components/spotify/Spotify";
-import ShapeGrid from "./components/reactbits/ShapeGrid";
-
 
 function App() {
   return (
@@ -19,29 +17,15 @@ function App() {
       className="w-screen p-0 m-0 bg-main-color justify-start
       flex flex-col relative"
     >
-
       <ReactLenis root />
-      <div className="w-full h-full -z-[1] bg-emerald-100 absolute top-0 left-0">
-        <ShapeGrid
-        speed={0.28}
-        squareSize={40}
-        direction="diagonal" // up, down, left, right, diagonal
-        borderColor="#18181a"
-        hoverFillColor="#222"
-        shape="hexagon" // square, hexagon, circle, triangle
-        hoverTrailAmount={9} // number of trailing hovered shapes (0 = no trail)
-      />
 
-      </div>
-
-      {
-      /*
+      {/*
         <div className="w-full flex justify-center h-fit border-b border-gray-800">
           <div className="w-[60%] flex justify-center border-r border-l border-gray-800 transition-all duration-200 ease-in-out max-lg:w-[95%] max-2xl:w-[80%]">
             <Navigation />
           </div>
-        </div> */
-      }
+        </div> */}
+
       <div className="w-full flex justify-center h-fit border-b border-gray-800 ">
         <div className="w-[60%] flex justify-center border-r border-l border-gray-800 transition-all duration-200 ease-in-out max-lg:w-[97%] max-2xl:w-[80%]">
           <Info />
