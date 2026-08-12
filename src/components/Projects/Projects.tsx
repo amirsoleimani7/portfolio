a6399e4fd696998ae131e937acc5928f33742282
amirsoleimani7 <a.soleimani@ec.iut.ac>
2026-08-12 16:16:11 +0330

    add : tecksack for project instance added

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,6 +2,7 @@ import { projects } from "../../storage/data/ProjectsInformation";
 import { useState } from "react";
 import { ProjectInstance } from "./ProjectInstance";
 import { FloatingPreview } from "./FloatingPreview";
+import { BsArrowReturnRight } from "react-icons/bs";
 
 export const Projects = () => {
   type postionType = {
@@ -49,17 +50,27 @@ export const Projects = () => {
       <div className="grid  w-full h-200 grid-cols-2 p-5 gap-1 max-lg:grid-cols-1">
         {projects.map((p, index) => {
           return (
-            <div className="p-5 border border-gray-800 *:select-none">
+            <div className="flex flex-col p-5 border border-gray-800 *:select-none group cursor-pointer duration-200 ease-in-out transition-all hover:border-gray-500">
               <div className="overflow-hidden">
                 <img
                   src={p.imageLink}
                   alt=""
-                  className=" object-cover ease-in-out transition-all duration-300  hover:scale-110 cursor-pointer"
+                  className=" object-cover ease-in-out transition-all duration-300  group-hover:scale-110 cursor-pointer"
                 />
               </div>
-              <div className="flex flex-col gap-1 mt-2">
+              <div className="flex flex-col gap-1 mt-2 mb-5">
                 <h1 className="text-white font-semibold">{p.projectName}</h1>
                 <p className="text-gray-400">{p.desc}</p>
+              </div>
+              <div className="flex gap-1 mt-auto mb-0">
+                {p.tecksUsed.map((t, index) => (
+                  <div
+                    className="text-gray-400 px-2 py-1 rounded-2xl border border-gray-800 hover:text-white  hover:border-white duration-200 transition-all ease-in-out "
+                    key={index}
+                  >
+                    {t.text}
+                  </div>
+                ))}
               </div>
             </div>
           );
